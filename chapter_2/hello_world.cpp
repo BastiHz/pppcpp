3d693cfb1b0c54f1519202ed0b3e46d7023244c7
Sebastian Henz <41594266+BastiHz@users.noreply.github.com>
2019-04-08 10:09:30 +0200

    Update hello_world.cpp

--- chapter_2/hello_world.cpp
+++ chapter_2/hello_world.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
-using namespace std;
 
 // Ich nutze die header-Datei aus dem Buch nicht, 
 // da sie veraltet ist und Warnungen und Fehlermeldungen erzeugt.
+// Außerdem nutze ich "using namespace std;" nicht, da es als
+// bad practice angesehen wird.
 
 int main() {
-    cout<<"Hello, World!\n";
+    std::cout<<"Hello, World!\n";
     return 0;
 }
